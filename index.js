16011920c8e23e85317f6b292cb465b8a70a41a8
Sebastian Raju <sebzstianraju79@gmail.com>
2025-02-23 20:34:07 +0530

    episode 3- jsx, babel, react functional components

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+const heading = React.createElement("h1", { id: "heading" }, "Namaste React");
+
+// jsx is transpiled by babel to react element => react element gets converted to html element after root.render
+const jsxHeading = <h1>This is namaste react !!!</h1>;
+
+console.log(jsxHeading);
+
+const newHeading = <div>React Element</div>;
+
+const NewFunctionalComponent = () => {
+  return <div>New Functional Component</div>;
+};
+
+const HeadingComponent = () => {
+  return (
+    <div>
+      {newHeading}
+
+      {NewFunctionalComponent()}
+      <NewFunctionalComponent></NewFunctionalComponent>
+      <NewFunctionalComponent />
+      <div>Hello world</div>
+    </div>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const parent = React.createElement(
-  "div",
-  { id: "Parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", { key: "h1" }, "Hi I am H1 Tag"),
-    React.createElement("h2", { key: "h2" }, "Hi I am H2 Tag"),
-  ])
-);
-
-root.render(parent);
+root.render(<HeadingComponent />);
